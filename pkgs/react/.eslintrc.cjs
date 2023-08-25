32ace8f85a1efadaa9ed6cdb6fd4bd00fc6912c7
chvndler <chvndler.ch@icloud.com>
2023-08-25 18:00:58 -0400

    upd

--- pkgs/react/.eslintrc.cjs
+++ pkgs/react/.eslintrc.cjs
@@ -4,21 +4,13 @@ module.exports = {
   env: { node: true, browser: true },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2021, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module', // Allows for the use of imports and exports
+    ecmaVersion: 2021,
+    sourceType: 'module',
     ecmaFeatures: {
-      jsx: true, // allows for the parsing of JSX
+      jsx: true,
     },
-    tsconfigRootDir: __dirname, // root directory for relative tsconfig paths
-    project: ['./tsconfig.json'], // specify it only if you want to type-check
+    project: './tsconfig.json',
     warnOnUnsupportedTypeScriptVersion: true,
   },
-  /**
-   * not 100% sure if these need to be set,
-   * because we are extending the atlrdsgn config
-   * which already has these set.
-   *
-   * They in fact do not need to be set.
-   */
   rules: {},
 };
