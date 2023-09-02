a7f3485b633e2216ebfbc4a5e2fed6da2fde4f26
chvndler <chvndler.ch@icloud.com>
2023-09-02 01:36:33 -0400

    Update _app.tsx

--- apps/docs/src/pages/_app.tsx
+++ apps/docs/src/pages/_app.tsx
@@ -1,20 +1,8 @@
 import './index.css';
 
-import { MonaSans } from '@/lib/fonts';
-
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <style
-        jsx
-        global>
-        {`
-          :root {
-            font-size: 0.9em;
-            --font-mona-sans: ${MonaSans.style.fontFamily};
-          }
-        `}
-      </style>
       <Component {...pageProps} />
     </>
   );
