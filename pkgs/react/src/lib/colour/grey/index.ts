d2bbdf9a5e5e3c3b8a087facbae2a096c462123e
chvndler <chvndler.ch@icloud.com>
2023-10-14 16:30:06 -0400

    upd

--- pkgs/react/src/lib/colour/grey/index.ts
+++ pkgs/react/src/lib/colour/grey/index.ts
@@ -29,3 +29,18 @@ export const greyDark = {
   grey11: '#f8f8f8',
   grey12: '#fcfcfc',
 } as const;
+
+/* Blue Grey
+[
+  "#f3f3fe",
+  "#e4e6ed",
+  "#c8cad3",
+  "#a9adb9",
+  "#9093a4",
+  "#808496",
+  "#767c91",
+  "#656a7e",
+  "#585e72",
+  "#4a5167"
+]
+*/
